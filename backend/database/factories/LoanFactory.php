67aa3a4f935992d5b6c17f533643b151c7444a9c
kzacristo <profissionalchrisostomo@gmail.com>
2024-09-04 01:15:12 -0300

    fix:factories

---
+++ backend/database/factories/LoanFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Infra\Adapters\Persistence\Eloquent\Models\Loan;
+use App\Infra\Adapters\Persistence\Eloquent\Models\User;
+use App\Infra\Adapters\Persistence\Eloquent\Models\Book;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Loan>
+ */
+class LoanFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Loan::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => $this->faker->uuid(),
+            'user_id' => User::inRandomOrder()->first()->id,
+            'book_id' => Book::inRandomOrder()->first()->id,
+            'loan_date' => $this->faker->dateTimeBetween('-1 month', 'now'),
+            'return_date' => $this->faker->optional()->dateTimeBetween('now', '+1 month'),
+            'status' => $this->faker->randomElement(['loaned', 'returned']),
+        ];
+    }
+}
--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -2,8 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Infra\Adapters\Persistence\Eloquent\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -11,32 +11,24 @@ use Illuminate\Support\Str;
 class UserFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
+    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
     public function definition()
     {
+        $passwd = bcrypt('password') ?? '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi';
         return [
-            'name' => fake()->name(),
-            'email' => fake()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'id' => $this->faker->uuid(),
+            'name' => $this->faker->name(35),
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => $passwd,
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
     }
 }
